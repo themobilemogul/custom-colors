1bc724fbd8e45d310b2d0740f45c86f8dccaf6f3
Adam Smith <adamsmithy2000@live.com>
2025-08-06 21:06:26 -0400

    Style upgrades, checkout work flow

--- server/index.js
+++ server/index.js
@@ -147,7 +147,7 @@ app.post('/checkout', async (req, res) => {
       ],
       mode: 'payment',
       success_url: `https://www.customcolors.store/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `https://www.customcolors.store/cancel`,
+      cancel_url: `https://www.customcolors.store`,
 
       metadata: { downloadUrl }
     });
